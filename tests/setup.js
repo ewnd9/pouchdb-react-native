99086e76b1cbbe31168449d8f4fbc756bdd38c42
ewnd9 <ewndnine@gmail.com>
2016-08-25 15:34:47 +0800

    add original pouchdb repo as a submodule, setup unit tests on Travis

---
+++ tests/setup.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const pouchDir = require('path').resolve(__dirname + '/../pouchdb-original/packages/node_modules');
+
+require('babel-register')({
+  presets: ['react-native'],
+  ignore: name => {
+    return name.indexOf('node_modules') > -1 && name.indexOf(pouchDir) !== 0;
+  }
+});
+
+require('react-native-mock/mock');
+
+const fs = require('fs');
+const Module = require('module');
+
+const _require = Module.prototype.require;
+
+const reqPouch = () => _require.call(null, require.resolve('./pouchdb-for-coverage/')).default;
+const reqPouchModule = name => _require.call(null, require.resolve(`../pouchdb-original/packages/node_modules/${name}`));
+
+const map = fs
+  .readdirSync(__dirname + '/../pouchdb-original/packages/node_modules')
+  .reduce((total, name) => {
+    total[name] = reqPouchModule.bind(null, name);
+    return total;
+  }, {
+    '../../packages/node_modules/pouchdb-for-coverage': reqPouch,
+    '../../packages/node_modules/pouchdb': reqPouch
+  });
+
+Module.prototype.require = function patchedRequire(name) {
+  const callback = map[name];
+
+  if (callback) {
+    return callback();
+  }
+
+  return _require.call(this, name);
+};
+
+require('../pouchdb-original/tests/integration/node.setup.js');
